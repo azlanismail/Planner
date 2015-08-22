3861b06c0f5522cb4d9c82ab30c85b01ce04c34a
azlanismail <azlanismail08@gmail.com>
2015-08-22 13:14:55 +0100

    I am trying to write to a file.

--- PlanningComp/src/synthesis/PrismChecker.java
+++ PlanningComp/src/synthesis/PrismChecker.java
@@ -16,26 +16,47 @@ public class PrismChecker {
       public void go()
      {
            try {
-              PrismLog mainLog = new PrismFileLog("stdout" );
+              PrismLog mainLog = new PrismFileLog("stdout");
               Prism prism = new Prism(mainLog , mainLog );
-               prism.initialise();
+              prism.initialise();
+            
+              
+              //load the model with a constant value
               Values v = new Values();
-               v.setValue( "CYCLEMAX" , 2);
+              v.setValue( "CYCLEMAX" , 2);
               ModulesFile modulesFile = prism.parseModelFile( new File("./Prismfiles/mainmodel_v11.smg" ));
               modulesFile .setUndefinedConstants(v);
+              
+              //load the property
               PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile , new File("./Prismfiles/prop200815.props"));
               propertiesFile.setUndefinedConstants( null);
-              Model model = prism.buildModel(modulesFile );
-              Result result = prism.modelCheck( model, propertiesFile , propertiesFile .getProperty(0));
-              System. out .println(result .getResult());
+              Model model = prism.buildModel(modulesFile);
+              Result result = prism.modelCheck( model, propertiesFile , propertiesFile.getProperty(0));
+            //  System.out.println(result.getResult());
+              System.out.println("testing");
+              
+              //write the outcomes into a file
+              File f = new File("./myfile.txt");
+              if(f.createNewFile())
+                  System.out.println("Success!");
+              else
+                  System.out.println("Error, file already exists.");
+              
+              int expT = 1; 
+              //prism.exportStatesToFile(model, expT, f);
           }
            catch (FileNotFoundException e ) {
-              System. out .println("Error: " + e .getMessage());
+              System. out .println("Error: " + e.getMessage());
               System. exit(1);
           }
            catch (PrismException e ) {
-              System. out .println("Error: " + e .getMessage());
+              System. out .println("Error: " + e.getMessage());
               System. exit(1);
+          }
+           catch(IOException e) {
+              // ioe.printStackTrace();
+               System. out .println("Error: " + e.getMessage());
+               System. exit(1);
           }
      }
 }
