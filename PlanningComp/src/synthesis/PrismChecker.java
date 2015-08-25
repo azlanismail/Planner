833e28014486ed651b48da0c405c57fc211f1bc5
azlanismail <azlanismail08@gmail.com>
2015-08-25 16:47:32 +0100

    another attempt to extract data into a file.

--- PlanningComp/src/synthesis/PrismChecker.java
+++ PlanningComp/src/synthesis/PrismChecker.java
@@ -4,16 +4,23 @@ import java.io.*;
 import parser.Values;
 import parser.ast.*;
 import prism.*;
+//import prism.Model;
+//import explicit.*;
 
 public class PrismChecker {
 
       public static void main(String[] args) {
            // TODO Auto-generated method stub
-          new PrismChecker().go();
+    	  PrismChecker pc = new PrismChecker();
+          pc.synthesis();
      }
      
+      public PrismChecker()
+      {
+    	  
+      }
 
-      public void go()
+      public void synthesis()
      {
            try {
               PrismLog mainLog = new PrismFileLog("./myLog.txt");
@@ -23,28 +30,29 @@ public class PrismChecker {
               
               //load the model with a constant value
               Values v = new Values();
-              v.setValue( "CYCLEMAX" , 2);
-              ModulesFile modulesFile = prism.parseModelFile( new File("./Prismfiles/mainmodel_v11.smg" ));
-              modulesFile .setUndefinedConstants(v);
+              v.setValue("CYCLEMAX" , 2);
+              ModulesFile modulesFile = prism.parseModelFile( new File("./Prismfiles/smg_example.prism"));
+              modulesFile.setUndefinedConstants(v);
               
               //load the property
-              PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile , new File("./Prismfiles/prop200815.props"));
+              PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile , new File("./Prismfiles/smg_example.props"));
               propertiesFile.setUndefinedConstants( null);
               Model model = prism.buildModel(modulesFile);
-            //  Result result = prism.modelCheck( model, propertiesFile , propertiesFile.getProperty(0));
-            //  System.out.println(result.getResult());
-              System.out.println("testing");
+              Result result =  prism.modelCheck(model, propertiesFile , propertiesFile.getProperty(0));
+              // System.out.println("Number of transition is"+ model);
+              System.out.println("Number of transition is"+ result.getExplanation());
+              // System.out.println("testing");
               
-              //write the outcomes into a file
-              File f = new File("./myfile.txt");
-              if(f.createNewFile())
-                  System.out.println("Success!");
-              else
-                  System.out.println("Error, file already exists.");
+              // write the outcomes into a file
+              // File f = new File("./myfile.txt");
+              //    if(f.createNewFile())
+              //      System.out.println("Success!");
+              //    else
+              //       System.out.println("Error, file already exists.");
               
-              int expT = 1; 
-              //prism.exportStatesToFile(model, expT, f);
-              prism.exportPRISMModel(f);
+              //   int expT = 1; 
+              //   prism.exportStateRewardsToFile(model, expT, f);
+              //   prism.exportPRISMModel(f);
           }
            catch (FileNotFoundException e ) {
               System. out .println("Error: " + e.getMessage());
