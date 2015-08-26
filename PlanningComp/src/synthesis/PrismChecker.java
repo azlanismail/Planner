6fdd40c527d90ba31f3ba2014493bbdfd31434c8
azlanismail <azlanismail08@gmail.com>
2015-08-26 15:32:29 +0100

    updated at 3.32pm on 26/8/15

--- PlanningComp/src/synthesis/PrismChecker.java
+++ PlanningComp/src/synthesis/PrismChecker.java
@@ -4,8 +4,6 @@ import java.io.*;
 import parser.Values;
 import parser.ast.*;
 import prism.*;
-//import prism.Model;
-//import explicit.*;
 
 public class PrismChecker {
 
@@ -30,41 +28,41 @@ public class PrismChecker {
               
               //load the model with a constant value
               Values v = new Values();
-              v.setValue("CYCLEMAX" , 2);
-              ModulesFile modulesFile = prism.parseModelFile( new File("./Prismfiles/smg_example.prism"));
-              modulesFile.setUndefinedConstants(v);
+              v.setValue("x" , 2);
+              ModulesFile modulesFile = prism.parseModelFile( new File("./Prismfiles/dice.pm"));
+              modulesFile .setUndefinedConstants(null);
               
               //load the property
-              PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile , new File("./Prismfiles/smg_example.props"));
-              propertiesFile.setUndefinedConstants( null);
+              PropertiesFile propertiesFile = prism.parsePropertiesFile(modulesFile , new File("./Prismfiles/dice.pctl"));
+              propertiesFile.setUndefinedConstants(v);
+      
               Model model = prism.buildModel(modulesFile);
-              Result result =  prism.modelCheck(model, propertiesFile , propertiesFile.getProperty(0));
-              // System.out.println("Number of transition is"+ model);
-              System.out.println("Number of transition is"+ result.getExplanation());
-              // System.out.println("testing");
+              Result result = prism.modelCheck( model, propertiesFile , propertiesFile.getProperty(0));
+              System.out.println(result.getResult());
+              System.out.println("testing");
               
               // write the outcomes into a file
-              // File f = new File("./myfile.txt");
-              //    if(f.createNewFile())
-              //      System.out.println("Success!");
-              //    else
-              //       System.out.println("Error, file already exists.");
+              File f = new File("./myfile.txt");
+                  if(f.createNewFile())
+                    System.out.println("Success!");
+                  else
+                     System.out.println("Error, file already exists.");
               
-              //   int expT = 1; 
-              //   prism.exportStateRewardsToFile(model, expT, f);
+              int expT = 1; 
+              prism.exportStateRewardsToFile(model, expT, f);
               //   prism.exportPRISMModel(f);
           }
            catch (FileNotFoundException e ) {
-              System. out .println("Error: " + e.getMessage());
+              System.out.println("Error: " + e.getMessage());
               System. exit(1);
           }
            catch (PrismException e ) {
-              System. out .println("Error: " + e.getMessage());
+              System.out.println("Error: " + e.getMessage());
               System. exit(1);
           }
            catch(IOException e) {
               // ioe.printStackTrace();
-               System. out .println("Error: " + e.getMessage());
+               System.out.println("Error: " + e.getMessage());
                System. exit(1);
           }
      }
