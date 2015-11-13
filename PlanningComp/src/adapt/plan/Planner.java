cb46c2b47a504b1da48549be2f5b0c702c20658c
azlanismail <azlanismail08@gmail.com>
2015-11-13 22:01:05 +0000

    updated 13/11

--- PlanningComp/src/adapt/plan/Planner.java
+++ PlanningComp/src/adapt/plan/Planner.java
@@ -51,7 +51,7 @@ public class Planner {
 	String logPath = "./myLog.txt";
 	String laptopPath = "C:\\Users\\USER\\";
 	String desktopPath = "H:\\";
-	String mainPath = desktopPath;
+	String mainPath = laptopPath;
 	String modelPath = mainPath+"git\\Planner\\PlanningComp\\Prismfiles\\teleAssistance_v2.smg";
 	String propPath = mainPath+"git\\Planner\\PlanningComp\\Prismfiles\\propTeleAssistance.props";
 	String modelConstPath = mainPath+"git\\Planner\\PlanningComp\\IOFiles\\ModelConstants.txt";
